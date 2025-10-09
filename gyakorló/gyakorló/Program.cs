f6b4d99903676a28820e1df5ed7bfe55c39b84f9
susyhuni <solyomh@kkszki.hu>
2025-10-09 10:14:40 +0200

    gyakorló p1

---
+++ gyakorló/gyakorló/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakorló
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan now = DateTime.Now.TimeOfDay;
+            TimeSpan regg_s = new TimeSpan(6, 0, 0);
+            TimeSpan regg_e = new TimeSpan(10, 0, 0);
+            TimeSpan délE_s = new TimeSpan(10, 0, 0);
+            TimeSpan délE_e = new TimeSpan(12, 0, 0);
+            TimeSpan dél = new TimeSpan(12, 0, 0);
+            TimeSpan délU_s = new TimeSpan(12, 0, 0);
+            TimeSpan délU_e = new TimeSpan(19, 0, 0);
+            TimeSpan este_s = new TimeSpan(19, 0, 0);
+            TimeSpan este_e = new TimeSpan(6, 0, 0);
+
+            if ((now >= regg_s) && (now < regg_e))
+            {
+                Console.WriteLine("Jó regget!");
+            }
+
+            if ((now >= délE_s) && (now < délE_e))
+            {
+                Console.WriteLine("Dél előtt van!");
+            }
+
+            if (now == dél)
+            {
+                Console.WriteLine("Dél van!");
+            }
+
+            if ((now > délU_s) && (now < délU_e))
+            {
+                Console.WriteLine("Dél után van!");
+            }
+            if ((now >= este_s) && (now < este_e))
+            {
+                Console.WriteLine("Jó estét!");
+            }
+        }
+    }
+}
+
